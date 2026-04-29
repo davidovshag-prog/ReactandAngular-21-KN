8faa81c8d8d63f0c848a0759255567830819d453
Roman <davidovshag@gmail.com>
2026-04-29 21:14:01 +0300

    Updated Project

--- my-redux/src/store/index.ts
+++
@@ -1,6 +0,0 @@
-import {createStore} from "redux"; // просто підказує, що є стрішки застарілий спосіб
-import counterReducer from "../reducers/counterReducer.ts";
-
-const store = createStore(counterReducer);
-
-export default store;
--- my-redux/src/types/actionTypes.ts
+++
@@ -1,3 +0,0 @@
-// src/redux/actionTypes.js
-export const INCREMENT = 'INCREMENT'; // Збільшити на 1 або менше
-export const DECREMENT = 'DECREMENT'; // Зменшити на 1 або менше
